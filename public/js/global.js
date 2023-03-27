0dee66a1cd7baae9c1e81ae1defb02f685a49f33
itsmeit <buivanloi.2010@gmail.com>
2023-03-28 02:26:10 +0700

    --

--- public/js/global.js
+++ public/js/global.js
@@ -1,21 +1,38 @@
 (function ($) {
     'use strict';
 
+    var isLinkReady = false;
+    var isCountdownRunning = false;
+
     $(function () {
         var $urls = $('a');
         var end_point = $.trim(prep_vars.end_point);
         var prep_url = prep_vars.prep_url;
         var current_url = window.location.href.replace(/#.*/, '');
-        var count_down   = $.trim(prep_vars.count_down);
+        var count_down = parseInt(prep_vars.count_down);
 
-        let isLinkReady = false;
-        let isCountdownRunning = false;
 
+        const updateLink = (href) => {
+            if (current_url.indexOf('?') !== -1) {
+                current_url = current_url.split('?')[0];
+            }
+            if (window.location.href.indexOf(".html") > -1) {
+                if (current_url.includes('/' + end_point)) {
+                    current_url = current_url.replace('/' + end_point, '');
+                }
+                $urls.attr('href', `${current_url + '/' + end_point}`);
+                $urls.attr('data-id', `${current_url + '/' + end_point}`);
+            } else {
+                $urls.attr('href', `${current_url + end_point}`);
+                $urls.attr('data-id', `${current_url + end_point}`);
+            }
+            document.cookie = "pre_url_go=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+            document.cookie = "pre_url_go=" + encodeURIComponent(href) + "; path=/;";
+        }
 
         const startCountdown = ($link, href, text_link) => {
             let downloadTimer;
             let timeleft = count_down;
-
             isCountdownRunning = true;
 
             const countdown = () => {
@@ -24,37 +41,16 @@
                 if (timeleft < 0) {
                     clearInterval(downloadTimer);
                     $link.removeAttr('target');
-
-                    if (current_url.indexOf('?') !== -1) {
-                        current_url = current_url.split('?')[0];
-                    }
-
-                    if (window.location.href.indexOf(".html") > -1) {
-                        if (current_url.includes('/' + end_point)) {
-                            current_url = current_url.replace('/' + end_point, '');
-                        }
-                        $link.attr('href', `${current_url + '/' + end_point}`);
-                        $link.attr('data-id', `${current_url + '/' + end_point}`);
-                    } else {
-                        $link.attr('href', `${current_url + end_point}`);
-                        $link.attr('data-id', `${current_url + end_point}`);
-                    }
-
                     $link.text(`${text_link} (Ready!)`);
                     isLinkReady = true;
                     isCountdownRunning = false;
-
-                    document.cookie = "pre_url_go=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-                    // set cookie
-                    document.cookie = "pre_url_go=" + encodeURIComponent(href) + "; path=/;";
-
+                    updateLink(href);
                 } else {
                     setTimeout(countdown, 1000);
                 }
             }
-
             countdown();
-        };
+        }
 
         $urls.on('click', function (e) {
             const $this = $(this);
@@ -62,18 +58,24 @@
             const text_link = $this.text();
             const href = $this.attr('href');
             const prep_urls = prep_url.replace(/\\r\\|\r\n|\s/g, "").replace(/^,|,$/g, '').split(",");
-
             if (btnDownload || href === undefined || href === null || !href.length) {
                 return;
             }
 
             let isPrevented = false;
 
+            if (count_down === 0) {
+                e.preventDefault();
+                updateLink(href);
+                window.location.href = $this.attr('href');
+                return;
+            }
+
             $.each(prep_urls, function (key, text) {
-                if (href.includes($.trim(text)) || href.includes(text.trim())) {
-                    if (isLinkReady || isCountdownRunning) {
+                if (href.includes($.trim(text))) {
+                    if (isLinkReady || (isCountdownRunning && $(this).is($this))) {
                         e.preventDefault();
-                        if (isCountdownRunning) {
+                        if (isCountdownRunning && !$(this).is($this)) {
                             alert("Please wait for the countdown to finish before clicking again.");
                         } else {
                             window.location.href = $this.attr('href');
@@ -92,6 +94,5 @@
                 isLinkReady = false;
             }
         });
-
     });
 })(jQuery);
