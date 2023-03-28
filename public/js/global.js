35ce0826540bd198f1a9b4e9b999462c77273455
itsmeit <buivanloi.2010@gmail.com>
2023-03-29 01:04:09 +0700

    update, change logic link click

--- public/js/global.js
+++ public/js/global.js
@@ -1,9 +1,6 @@
 (function ($) {
     'use strict';
 
-    var isLinkReady = false;
-    var isCountdownRunning = false;
-
     $(function () {
         var $urls = $('a');
         var end_point = $.trim(prep_vars.end_point);
@@ -23,40 +20,46 @@
                 $link.attr('data-id', `${current_url + '/' + end_point + '?id=' + href}`);
             } else {
                 $link.attr('href', `${current_url + end_point + '?id=' + href}`);
-                $link.attr('data-id', `${current_url + end_point + '?id=' + href }`);
+                $link.attr('data-id', `${current_url + end_point + '?id=' + href}`);
             }
         }
 
         const startCountdown = ($link, href, text_link) => {
-            var isLinkReady = false;
-            var isCountdownRunning = false;
-
             let downloadTimer;
             let timeleft = time_cnf;
-            isCountdownRunning = true;
+            const $progress = $link.find('.progress');
+            const progressWidth = $progress.outerWidth() + 10;
+
+            if (text_link.length > 35) {
+                text_link = text_link.substring(0, text_link.length - 15) + '...';
+            }
 
             const countdown = () => {
+
+                // $progress.animate({width: progressWidth}, (timeleft * 1000) / 2, function () {
+                //     // $link.html('<span class="progress" style="color:red;">'+`${text_link}`+ '..' +'[Ready!]'+'</span>');
+                // });
                 $link.html(`<strong>[ waiting ${timeleft}s... ]</strong>`);
                 timeleft--;
                 if (timeleft < 0) {
                     clearInterval(downloadTimer);
                     $link.removeAttr('target');
-                    $link.html(`${text_link}` + '<strong style="color:red;">(Ready!)</strong>');
-                    isLinkReady = true;
-                    isCountdownRunning = false;
+                    $link.html('<span class="progress" style="color:red;">' + `${text_link}` + '[Link ready!]' + '</span>');
                     updateLink($link, href);
                 } else {
                     setTimeout(countdown, 1000);
                 }
-            }
-            countdown();
-        }
+            };
 
-        $urls.each(function() {
+            countdown();
+        };
+
+
+        $urls.each(function () {
             var href = $(this).attr('href');
             var prep_urls = prep_url.replace(/\\r\\|\r\n|\s/g, "").replace(/^,|,$/g, '').split(",");
             var found = false;
-
+            var text_link = $(this).text();
             const btnDownload = $(this).hasClass('prep-link-download-btn');
             if (btnDownload || href === undefined || href === null || !href.length) {
                 return;
@@ -73,11 +76,15 @@
                 var encoded_link = btoa(href);
                 $(this).attr('href', encoded_link);
                 $(this).attr('data-id', encoded_link);
+                // $(this).addClass('progress');
+                $(this).html('<span class="progress">' + text_link + '</span>')
+                $(this).removeAttr('target');
             }
         });
 
         $urls.on('click', function (e) {
             const $this = $(this);
+
             const btnDownload = $this.closest('.prep-link-download-btn').hasClass('prep-link-download-btn');
             const text_link = $this.text();
             const href = $this.attr('href');
@@ -85,8 +92,6 @@
             if (btnDownload || href === undefined || href === null || !href.length) {
                 return;
             }
-
-            let isPrevented = false;
 
             if (time_cnf === 0) {
                 e.preventDefault();
@@ -98,7 +103,6 @@
             var isBtoaEncoded = false;
 
             if (time_cnf > 0) {
-
                 try {
                     var decodedHref = atob(href);
                     isBtoaEncoded = decodedHref.match(/^https?:\/\/.+/) !== null;
@@ -113,11 +117,6 @@
                 $this.off('click').attr('href', 'javascript:void(0);');
                 startCountdown($this, href, text_link);
             }
-
-            if (!isPrevented && isLinkReady) {
-                isLinkReady = false;
-            }
         });
-
     });
 })(jQuery);
