1f3aa9f01487610d1a81ab2d49f01c6467f0bd30
itsmeit <buivanloi.2010@gmail.com>
2023-11-02 23:16:26 +0700

    fix cookie

--- public/js/clear-cookie.js
+++ public/js/clear-cookie.js
@@ -8,18 +8,22 @@
 
     $(function () {
         var end_point = $.trim(cookie_vars.end_point),
-            current_url = window.location.href;
+            current_url = window.location.href.replace(/#.*/, '');
+        var regex = new RegExp('(/' + end_point + '/)|(/' + end_point + ')|(.html/' + end_point + ')');
 
         function deleteCookie(cookieName) {
             document.cookie = cookieName + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
         }
 
         function _clear_cookie(){
-            if (!current_url.endsWith("/" + end_point) || !current_url.endsWith("/" + end_point + '/')) {
+            if (regex.test(current_url)) {
+                return true;
+            } else {
                 deleteCookie("prep_link_href");
                 deleteCookie("prep_text_link");
             }
         }
+
         _clear_cookie();
     });
 })(jQuery);
